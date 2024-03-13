586f997070e79fcd25e5eeea94d94a503915cb39
Daniel Melo <danielfmelo21@gmail.com>
2024-03-12 21:56:11 -0500

    rewrite merge sort

--- merge_sort/Main.java
+++ merge_sort/Main.java
@@ -1,12 +1,12 @@
 package merge_sort;
 
-import java.util.Arrays;;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         MergeSort mergeSort = new MergeSort();
-        int[] result = mergeSort.mergeSort(new int[] {6,3,7,8,2,634,5,28,88});
+        int[] merged = mergeSort.mergeSort(new int[] { 8, 40, 20, 7, 4, 83, 2, 94, 75, 39, 29, 2, 52, 44 });
 
-        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(merged));
     }
 }
--- merge_sort/MergeSort.java
+++ merge_sort/MergeSort.java
@@ -4,40 +4,40 @@ import java.util.Arrays;
 
 public class MergeSort {
     
-    public int[] merge(int[] nums1, int[] nums2) {
-        int[] result = new int[nums1.length + nums2.length];
+    private int[] merge(int[] array1, int[] array2) {
+        int[] combined = new int[array1.length + array2.length];
         int index = 0, i = 0, j = 0;
 
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
-                result[index] = nums1[i];
+        while (i < array1.length && j < array2.length) {
+            if (array1[i] < array2[j]) {
+                combined[index] = array1[i];
                 i++;
             } else {
-                result[index] = nums2[j];
+                combined[index] = array2[j];
                 j++;
             }
 
             index++;
         }
 
-        while (i < nums1.length) {
-            result[index] = nums1[i];
+        while(i < array1.length) {
+            combined[index] = array1[i];
             i++;
             index++;
         }
 
-        while (j < nums2.length) {
-            result[index] = nums2[j];
+        while(j < array2.length) {
+            combined[index] = array2[j];
             j++;
             index++;
         }
 
-
-        return result;
+        return combined;
     }
 
     public int[] mergeSort(int[] array) {
         if (array.length <= 1) return array;
+
         int midIndex = array.length / 2;
 
         int[] left = mergeSort(Arrays.copyOfRange(array, 0, midIndex));
